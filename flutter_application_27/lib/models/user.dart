1312918f045c39f815591aafa0526a89225150eb
Sharif <garciatoledosharifalejandra@gmail.com>
2026-09-06 22:38:01 -0500

    Models de User y Hair

---
+++ flutter_application_27/lib/models/user.dart
@@ -0,0 +1,85 @@
+import 'package:flutter_application_27/models/hair.dart';
+
+class User {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String maidenName;
+  final int age;
+  final String gender;
+  final String email;
+  final String phone;
+  final String username;
+  final String password;
+  final String birthDate;
+  final String image;
+  final String bloodGroup;
+  final double height;
+  final double weight;
+  final String eyeColor;
+  final Hair hair;
+
+  User({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.maidenName,
+    required this.age,
+    required this.gender,
+    required this.email,
+    required this.phone,
+    required this.username,
+    required this.password,
+    required this.birthDate,
+    required this.image,
+    required this.bloodGroup,
+    required this.height,
+    required this.weight,
+    required this.eyeColor,
+    required this.hair,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      maidenName: json['maidenName'],
+      age: json['age'],
+      gender: json['gender'],
+      email: json['email'],
+      phone: json['phone'],
+      username: json['username'],
+      password: json['password'],
+      birthDate: json['birthDate'],
+      image: json['image'],
+      bloodGroup: json['bloodGroup'],
+      height: (json['height'] as num).toDouble(),
+      weight: (json['weight'] as num).toDouble(),
+      eyeColor: json['eyeColor'],
+      hair: Hair.fromJson(json['hair']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'firstName': firstName,
+      'lastName': lastName,
+      'maidenName': maidenName,
+      'age': age,
+      'gender': gender,
+      'email': email,
+      'phone': phone,
+      'username': username,
+      'password': password,
+      'birthDate': birthDate,
+      'image': image,
+      'bloodGroup': bloodGroup,
+      'height': height,
+      'weight': weight,
+      'eyeColor': eyeColor,
+      'hair': hair.toJson(),
+    };
+  }
+}
